102c9496342079f49fe2abb33b2fa24e51045f73
Georges Haidar <ghaidar0@gmail.com>
2025-05-28 17:17:48 +0000

    checkpoint

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,31 +7,28 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: import.meta.env.VITE_SITE,
-
   vite: {
     server: {
       allowedHosts: ["localhost", "127.0.0.1", "devbox"],
     },
-
     plugins: [tailwindcss()],
   },
   integrations: [
     starlight({
       title: "Gram",
+      tagline: "Ship powerful integrations for agents and LLMs ",
       social: [
         {
           icon: "github",
           label: "GitHub",
-          href: "https://github.com/speakeasy-api/gram",
+          href: "https://github.com/speakeasy-api/gram-docs",
         },
       ],
       components: {
+        Header: "./src/components/header.astro",
         Hero: "./src/components/hero.astro",
+        Pagination: "./src/components/pagination.astro",
       },
-      // logo: {
-      //   light: "./src/assets/logo-light.png",
-      //   dark: "./src/assets/logo-dark.png",
-      // },
       editLink: {
         baseUrl: "https://github.com/speakeasy-api/gram/edit/main/docs/",
       },
